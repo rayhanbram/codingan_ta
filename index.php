714755a31f2cc41e90fcca4c96d25b4b643e85f8
M Rayhan Brameswara <mrbrameswara@gmail.com>
2022-07-20 13:57:44 +0700

    terbaru

--- index.php
+++ index.php
@@ -12,8 +12,51 @@ include('db_connection.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MamamYuk of Web IT</title>
+    <title>MamamYuk</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+img {
+  width: 40%;
+  height: 100px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.cardmenu{
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            min-width: 0;
+            word-wrap: break-word;
+            background-color: #fff;
+            background-clip: border-box;
+            border: 1px solid rgba(0, 0, 0, 0.125);
+            border-radius: 0.25rem;
+            margin-bottom: 10px;
+            margin-top: 10px;
+        }
+        .column {
+  float: left;
+  width: 50%;
+  padding: 0 20px;
+}
+
+.row {margin: 0 -5px;}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 0px;
+  }
+}
+</style>
 </head>
 <body>
     
@@ -22,7 +65,8 @@ include('db_connection.php');
             <div class="col-md-12">
                 <div class="card mt-3">
                     <div class="card-header">
-                        <h4>How to Filter or Find or Search data using Multiple Checkbox in php</h4>
+                      <button type="submit" class="btn btn-primary btn-sm float-end">Home</button>
+                        <h4>Rekomendasi Menu MPASI</h4>
                     </div>
                 </div>
             </div>
@@ -130,6 +174,7 @@ include('db_connection.php');
             <div class="col-md-9 mt-3">
                 <div class="card ">
                     <div class="card-body row">
+                    <h5>Makanan Utama</h5>
                         <?php
 							
 							if(isset($_GET['umur'])) $umur = $_GET['umur'];
@@ -137,7 +182,7 @@ include('db_connection.php');
 							if(isset($_GET['jenis'])) $prefs = $_GET['jenis'];
 							
 							
-							$query = "select distinct  a.menu_id id, a.menu menu, b.umur umur, c.deskripsi kategori from 
+							$query = "select distinct  a.menu_id id, a.menu menu, a.gambar, b.umur umur, c.deskripsi kategori from 
 										menu_mpasi a 
 										join umur b on a.umur_id = b.umur_id
 										join kategori c on a.kategori_id = c.kategori_id
@@ -167,12 +212,15 @@ include('db_connection.php');
 								foreach($menu_run as $menuitems) :
 									
 									   echo' <div>
-											<div style="border:1px solid #ccc; border-radius:5px; padding:16px; margin-bottom:16px; height:120px;">
-										   
-											<p align="center"><strong><a href="#">'. $menuitems['menu'] .'</a></strong></p>
+                                       <div class="column">
+											<div class="cardmenu">
+                                            
+											<p align="center"><strong><a href="detail.php?menu_id='.$menuitems['menu'].'&umur='.$menuitems['umur'].'&kategori='.$menuitems['kategori'].'&gambar='.$menuitems['gambar'].'">'. $menuitems['menu'] .'</a></strong></p>
+                                            <img src='. $menuitems['gambar'] .' alt="Gambar Menu" class="img">
 											<h6 style="text-align:center;" class="text-danger" >'. $menuitems['kategori'] .'</h6>
 											<p style="text-align:center;">Umur : '. $menuitems['umur'].' <br />
 										</div>
+                                        </div>
 									';
 								endforeach;
 							}
